ae6f1c067b587f5e0c3c6e58011a0dc7809f5a42
Rudolf Sargsyan <rudolf.sargsyan@ukg.com>
2022-08-31 22:03:03 +0400

    Initial commit

---
+++ src/main/java/com/epam/edumanagemnentsystem/EdumanagemnentsystemApplication.java
@@ -0,0 +1,13 @@
+package com.epam.edumanagemnentsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EdumanagemnentsystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EdumanagemnentsystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/epam/edumanagemnentsystem/EdumanagemnentsystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.epam.edumanagemnentsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EdumanagemnentsystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
