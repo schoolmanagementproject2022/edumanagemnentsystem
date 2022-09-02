857ebefc82694423593d1890638d6716e3b69138
Armine Zakaryan <armzak91@gmail.com>
2022-09-02 21:08:14 +0400

    Made changes in super admin initial files

--- src/main/java/com/epam/edumanagementsystem/admin/model/entity/SuperAdmin.java
+++ src/main/java/com/epam/edumanagementsystem/admin/model/entity/SuperAdmin.java
@@ -1,18 +1,23 @@
 package com.epam.edumanagementsystem.admin.model.entity;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Entity
-@Table
+@Table(name = "super_admin")
 public class SuperAdmin {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     private String email;
     private String password;
 
     public SuperAdmin() {
+    }
+
+    public SuperAdmin(int id, String email, String password) {
+        this.id = id;
+        this.email = email;
+        this.password = password;
     }
 
     public int getId() {
