8556697cfdadc52ce73a6821f4649b2acbf1174c
RafoMeliqyan <rafomelikyan10@gmail.com>
2022-09-06 22:27:42 +0400

    Added service impl.

--- src/main/java/com/epam/edumanagementsystem/parent/rest/service/ParentService.java
+++ src/main/java/com/epam/edumanagementsystem/parent/rest/service/ParentService.java
@@ -2,41 +2,18 @@ package com.epam.edumanagementsystem.parent.rest.service;
 
 import com.epam.edumanagementsystem.parent.model.dto.ParentDto;
 import com.epam.edumanagementsystem.parent.model.entity.Parent;
-import com.epam.edumanagementsystem.parent.rest.mapper.ParentMapper;
-import com.epam.edumanagementsystem.parent.rest.repository.ParentRepository;
-import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
-@Service
-public class ParentService {
+public interface ParentService {
 
-    private final ParentRepository parentRepository;
+    Parent findById(Long id);
 
-    public ParentService(ParentRepository parentRepository) {
-        this.parentRepository = parentRepository;
-    }
+    void save(ParentDto parent);
 
-    public Parent findById(Long id) {
-        Optional<Parent> byId = parentRepository.findById(id);
-        if (byId.isPresent()) {
-            return byId.get();
-        }
-        return null;
-    }
+    List<Parent> parents();
 
-    public void save(ParentDto parent) {
-        Parent savedParent = ParentMapper.toParent(parent);
-        parentRepository.save(savedParent);
-    }
-
-    public List<Parent> parents() {
-        return parentRepository.findAll();
-    }
-
-    public Optional<Parent> findByEmail(String email) {
-        return parentRepository.findByEmail(email);
-    }
+    Optional<Parent> findByEmail(String email);
 
 }
