65d12a82b82010f147d482df96f220ccdf3653fa
ZhenyaManukyan <zhenya-manukyan@inbox.ru>
2022-10-01 21:16:34 +0400

    SCHOOL-21: Added one statement in controller

--- src/main/java/com/epam/edumanagementsystem/admin/rest/api/AcademicCourseController.java
+++ src/main/java/com/epam/edumanagementsystem/admin/rest/api/AcademicCourseController.java
@@ -59,6 +59,8 @@ public class AcademicCourseController {
         if (result.hasErrors()) {
             if (result.hasFieldErrors("name")) {
                 return "academicCourseSection";
+            }else if (result.hasFieldErrors("subject")) {
+                return "academicCourseSection";
             }
         }
         academicCourseService.create(academicCourse);
