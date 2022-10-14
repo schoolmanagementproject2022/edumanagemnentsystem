5dd7396a8a7f06ee4b6ca7debbb4c99643c34341
Gohar Minasyan <goharminasyan80@gmail.com>
2022-10-14 14:09:54 +0400

    current commit

--- src/main/resources/static/js/popupControlTimetable.js
+++ src/main/resources/static/js/popupControlTimetable.js
@@ -84,9 +84,22 @@ function PopUpHide() {
     $("#show-btn").css("-webkit-filter", "blur(0px)");
 }
 
-function SuccessPopupShow() {
-    $("#popup-container").show();
+
+function save(){
+    //val dzerqov stex tal te html mej?
+    var inputValue=$('#1').val();
+    sessionStorage.setItem('save', inputValue);
+    location.href='/classes/3a/timetable';
+    SuccessPopupShow();
 }
+
+function SuccessPopupShow() {
+    var value = sessionStorage.getItem('save').valueOf();
+    if (value === "1") {
+        return $("#popup-container").show();
+    }
+}
+
 function SuccessPopupHide() {
     $("#popup-container").hide();
 }
