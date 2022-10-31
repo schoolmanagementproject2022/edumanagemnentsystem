3a9caa9a39c3c69d7a2ac8015f02b68b003cb092
Armine Zakaryan <armzak91@gmail.com>
2022-10-31 17:47:28 +0400

    Added @BeforeEach in SuperAdminRepositoryTest for instantiating a superAdmin instance used in all test methods

--- src/test/java/com/epam/edumanagementsystem/admin/rest/repository/SuperAdminRepositoryTest.java
+++ src/test/java/com/epam/edumanagementsystem/admin/rest/repository/SuperAdminRepositoryTest.java
@@ -1,31 +1,38 @@
 package com.epam.edumanagementsystem.admin.rest.repository;
 
 import com.epam.edumanagementsystem.admin.model.entity.SuperAdmin;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.test.context.TestPropertySource;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
-@TestPropertySource(properties = {"compile('org.flywaydb:flyway-core')"})
 class SuperAdminRepositoryTest {
 
     @Autowired
     private SuperAdminRepository superAdminRepository;
+    private SuperAdmin superAdmin;
+
+    @BeforeEach
+    public void setup() {
+        superAdmin = new SuperAdmin(1, "super-ADMIN@gmail.COM", "password");
+    }
 
     @Test
     void testFindByEmailIsPresentWithRightEmail() {
-        superAdminRepository.save(new SuperAdmin(1, "super-ADMIN@gmail.COM", "password"));
-        assertTrue(superAdminRepository.findByEmail("SUPER-adMin@GmaiL.coM").isPresent());
+        String email = "SUPER-adMin@GmaiL.coM";
+        superAdminRepository.save(superAdmin);
+        assertTrue(superAdminRepository.findByEmail(email).isPresent());
     }
 
     @Test
     void testFindByEmailIsEmptyWithWrongEmail() {
-        superAdminRepository.save(new SuperAdmin(1, "super-ADMIN@gmail.COM", "password"));
-        assertTrue(superAdminRepository.findByEmail("adDDDMin@maiL.co").isEmpty());
+        String email = "adDDDMin@maiL.co";
+        superAdminRepository.save(superAdmin);
+        assertTrue(superAdminRepository.findByEmail(email).isEmpty());
     }
 }
