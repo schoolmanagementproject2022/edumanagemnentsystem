a1a77ae86c0bc6f215131e2404daf5e7e39e9dae
Gohar Minasyan <goharminasyan80@gmail.com>
2022-11-02 17:00:27 +0400

    SCHOOL- 193 - refactor unit tests for TeacherServiceImpl.java

--- src/test/java/com/epam/edumanagementsystem/teacher/impl/TeacherServiceImplTest.java
+++ src/test/java/com/epam/edumanagementsystem/teacher/impl/TeacherServiceImplTest.java
@@ -1,6 +1,5 @@
-package com.epam.edumanagementsystem.teacher.rest.service;
+package com.epam.edumanagementsystem.teacher.impl;
 
-import com.epam.edumanagementsystem.teacher.impl.TeacherServiceImpl;
 import com.epam.edumanagementsystem.teacher.mapper.TeacherMapper;
 import com.epam.edumanagementsystem.teacher.model.dto.TeacherDto;
 import com.epam.edumanagementsystem.teacher.model.entity.Teacher;
@@ -8,12 +7,15 @@ import com.epam.edumanagementsystem.teacher.rest.repository.TeacherRepository;
 import com.epam.edumanagementsystem.util.entity.User;
 import com.epam.edumanagementsystem.util.exceptions.ObjectIsNull;
 import com.epam.edumanagementsystem.util.service.UserService;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
@@ -23,54 +25,63 @@ import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
-class TeacherServiceTest {
+class TeacherServiceImplTest {
     @Mock
     private TeacherRepository teacherRepository;
     @Mock
     private UserService userService;
     @InjectMocks
     private TeacherServiceImpl teacherService;
+    private User user;
+    private Teacher teacher;
 
-    @DisplayName("Check the usage of the findAll() method in the service")
+    @BeforeEach
+    void setUp() {
+        user = new User(null, "gm@gmail.com", "TEACHER");
+        teacher = new Teacher(1L, "Text", "Textyan", user, "password");
+    }
+
     @Test
+    @DisplayName("Check the usage of the findAll() method in the service")
     void canFindAllTeachers() {
-        // when
         teacherService.findAll();
-        // then
         verify(teacherRepository).findAll();
     }
-    @DisplayName("Check the usage of the findByUserId() method in the service")
+
     @Test
+    @DisplayName("The methode return  the right size of objects")
+    void findAll_returnRightObjects() {
+        when(teacherRepository.findAll()).thenReturn(List.of(teacher, teacher));
+        List<TeacherDto> allTeachers = teacherService.findAll();
+
+        assertThat(allTeachers).isNotNull();
+        assertThat(allTeachers.size()).isEqualTo(2);
+    }
+    @Test
+    @DisplayName("Check the usage of the findByUserId() method in the service")
     void findByUserId() {
-        // when
         teacherService.findByUserId(anyLong());
-        // then
         verify(teacherRepository).findByUserId(anyLong());
     }
 
-    @DisplayName("Create teacher positive case - given all parametrs")
     @Test
+    @DisplayName("Create teacher positive case - given all parametrs")
     void canCreateTeacher() {
-        //given
-        User user = new User(null, "gm@gmail.com", "TEACHER");
         when(userService.save(any())).thenReturn(user);
-        Teacher teacher = new Teacher(1L, "Text", "Textyan", user, "password");
         when(teacherRepository.save(any())).thenReturn(teacher);
         TeacherDto teacherDto = TeacherMapper.toDto(teacher);
 
-        //when
         Teacher createdTeacher = teacherService.create(teacherDto);
 
-        //then
         assertThat(createdTeacher).isNotNull();
         verify(teacherRepository).save(teacher);
         verify(userService).save(user);
     }
 
-    @DisplayName("Negative case for create methode - giving null to user")
     @Test
+    @DisplayName("Negative case for create methode - giving null to user")
     void canNotCreateTeacher() {
-      //then
         assertThrows(ObjectIsNull.class, () -> teacherService.create(null));
     }
+
 }
