e3dccdb5ee59b22a33ae8326124e9de62870ac4b
gohar <zakaryangohar49@gmail.com>
2022-11-03 20:24:35 +0400

    bag fix school 213

--- src/test/java/com/epam/edumanagementsystem/TestHelper.java
+++ src/test/java/com/epam/edumanagementsystem/TestHelper.java
@@ -1,5 +1,8 @@
 package com.epam.edumanagementsystem;
 
+import com.epam.edumanagementsystem.admin.model.dto.AdminDto;
+import com.epam.edumanagementsystem.admin.model.dto.SubjectDto;
+import com.epam.edumanagementsystem.admin.model.entity.Admin;
 import com.epam.edumanagementsystem.admin.model.entity.Subject;
 import com.epam.edumanagementsystem.teacher.model.entity.Teacher;
 import com.epam.edumanagementsystem.util.entity.User;
@@ -8,31 +11,80 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class TestHelper {
-    public static Subject createSubject() {
-        Subject subject = new Subject();
+    Subject subject = new Subject();
+    SubjectDto subjectDto = new SubjectDto();
+    Admin admin = new Admin();
+    AdminDto adminDto = new AdminDto();
+    User user = new User();
+
+    protected SubjectDto returnedSubject() {
+        subjectDto.setId(1L);
+        subjectDto.setName("SubjectName");
+        subjectDto.setTeacherSet(teacher());
+        return subjectDto;
+    }
+
+    protected Subject createSubject() {
         subject.setId(1L);
         subject.setName("SubjectName");
         subject.setTeacherSet(teacher());
         return subject;
     }
 
-    public static Subject updateSubject1() {
-        Subject subject = new Subject();
+    protected Subject updateSubject() {
         subject.setId(2L);
         subject.setName("Name_update");
         subject.setTeacherSet(teacher());
         return subject;
     }
 
-    public static Set<Teacher> teacher() {
+    protected Set<Teacher> teacher() {
         Set<Teacher> teacherSet = new HashSet<>();
         Teacher teacher = new Teacher();
         teacher.setId(1L);
         teacher.setName("teacher");
         teacher.setSurname("teacher");
         teacher.setPassword("teacher");
-        teacher.setUser(new User(1L, "teacher", "TEACHER"));
+        teacher.setUser(user());
         teacherSet.add(teacher);
         return teacherSet;
+    }
+
+    protected Admin input() {
+        admin.setId(1L);
+        admin.setUsername("admin");
+        admin.setSurname("adminyan");
+        admin.setPassword("admin");
+        admin.setUser(user());
+        return admin;
+
+    }
+
+    protected Admin returned() {
+        admin.setId(1L);
+        admin.setUsername("admin");
+        admin.setSurname("adminyan");
+        admin.setPassword("admin");
+        admin.setUser(user());
+        return admin;
+
+    }
+
+    protected AdminDto createAdmin() {
+        adminDto.setId(1L);
+        adminDto.setUsername("admin");
+        adminDto.setSurname("adminyan");
+        adminDto.setPassword("admin");
+        adminDto.setEmail("email@email.ru");
+        adminDto.setRole("ADMIN");
+        return adminDto;
+
+    }
+
+    protected User user() {
+        user.setId(1L);
+        user.setEmail("email@email.ru");
+        user.setRole("ADMIN");
+        return user;
     }
 }
