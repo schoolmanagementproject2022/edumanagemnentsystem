924ff9c776e1c373a2b89e777cf5dc975c4e85cf
Garik <45121072+gtepanosyan@users.noreply.github.com>
2022-11-07 14:01:25 +0400

    Unit Test Student

--- src/main/java/com/epam/edumanagementsystem/util/service/impl/UserServiceImpl.java
+++ src/main/java/com/epam/edumanagementsystem/util/service/impl/UserServiceImpl.java
@@ -14,11 +14,14 @@ import java.util.Optional;
 @Service
 public class UserServiceImpl implements UserService {
 
-    private final UserRepository userRepository;
+    private  UserRepository userRepository;
 
     @Autowired
     public UserServiceImpl(UserRepository userRepository) {
         this.userRepository = userRepository;
+    }
+
+    public UserServiceImpl() {
     }
 
     @Override
