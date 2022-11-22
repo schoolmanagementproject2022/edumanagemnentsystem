c60e0d03196631b4450f1a41f678a7f5bec8465e
Armine Zakaryan <armzak91@gmail.com>
2022-11-22 15:29:31 +0400

    Deleted subjectSectionForTeacherProfile css

--- src/main/java/com/epam/edumanagementsystem/teacher/rest/repository/TeacherRepository.java
+++ src/main/java/com/epam/edumanagementsystem/teacher/rest/repository/TeacherRepository.java
@@ -1,6 +1,5 @@
 package com.epam.edumanagementsystem.teacher.rest.repository;
 
-import com.epam.edumanagementsystem.admin.model.entity.Subject;
 import com.epam.edumanagementsystem.teacher.model.entity.Teacher;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
