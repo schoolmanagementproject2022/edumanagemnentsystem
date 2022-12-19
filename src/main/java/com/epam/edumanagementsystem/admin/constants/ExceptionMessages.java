1bef005ce7aff363a03d4232e555d35cbfc6df5d
ZhenyaManukyan <zhenya-manukyan@inbox.ru>
2022-12-19 21:39:37 +0400

    Refactoring admin

---
+++ src/main/java/com/epam/edumanagementsystem/admin/constants/ExceptionMessages.java
@@ -0,0 +1,14 @@
+package com.epam.edumanagementsystem.admin.constants;
+
+public class ExceptionMessages {
+
+    private static final String EMPTY_FIELD = "Please, fill the required fields";
+    private static final String ID_NULL = "Given id is null";
+    private static final String NAME_NULL = "The name is null";
+    private static final String ACADEMIC_COURSE_NULL = "Academic course is null";
+    private static final String NOT_PRESENT_COURSE_ID = "Does not present academic course by given id";
+    private static final String ACADEMIC_COURSE_BY_NAME = "Academic course by given name does not exist";
+    private static final String ACADEMIC_YEAR_BY_ID = "Did not find academic year by given id";
+    private static final String ACADEMIC_YEAR_NULL = "Academic year is null";
+
+}
---
+++ src/main/java/com/epam/edumanagementsystem/admin/constants/GlobalConstants.java
@@ -0,0 +1,8 @@
+package com.epam.edumanagementsystem.admin.constants;
+
+public class GlobalConstants {
+
+    private static final String MONDAY = "MONDAY";
+    private static final String DATE_FORMATTER = "yyyy-MM-dd";
+
+}
