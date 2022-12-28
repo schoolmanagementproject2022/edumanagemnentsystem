13d3940583a500a7bc9dc04407bd841af9105999
Armine Zakaryan <armzak91@gmail.com>
2022-12-28 12:36:42 +0400

    SCHOOL-334: Add components Added I18nConfig,AppConstants, messages.properties

---
+++ src/main/java/com/epam/edumanagementsystem/config/I18nConfig.java
@@ -0,0 +1,21 @@
+package com.epam.edumanagementsystem.config;
+
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+
+import java.util.Locale;
+
+@Configuration
+public class I18nConfig {
+
+    @Bean
+    public MessageSource messageSource(){
+        ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
+        messageSource.setBasename("classpath:messages");
+        messageSource.setDefaultEncoding("UTF-8");
+        messageSource.setDefaultLocale(new Locale("en"));
+        return messageSource;
+    }
+}
