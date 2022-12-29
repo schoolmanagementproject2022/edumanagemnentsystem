4850c9f73882d11430d1332ebd89b7121607dbba
Gohar Minasyan <goharminasyan80@gmail.com>
2022-12-29 20:13:25 +0400

    partly fixed bug school_341

--- src/main/java/com/epam/edumanagementsystem/admin/journal_agenda/rest/api/AgendaOfJournalController.java
+++ src/main/java/com/epam/edumanagementsystem/admin/journal_agenda/rest/api/AgendaOfJournalController.java
@@ -8,12 +8,15 @@ import com.epam.edumanagementsystem.admin.model.dto.AcademicClassDto;
 import com.epam.edumanagementsystem.admin.rest.service.AcademicClassService;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import javax.validation.Valid;
 import java.time.LocalDate;
 
 @Controller
@@ -36,14 +39,19 @@ public class AgendaOfJournalController {
     }
 
     @PostMapping("/add")
-    public String addAgenda(@ModelAttribute(value = "saveAgenda") SaveAgendaDto agendaDto,
-                            @RequestParam("concreteDay") String concreteDay,
+    public String addAgenda(@ModelAttribute(value = "saveAgenda") @Valid SaveAgendaDto agendaDto, BindingResult result,
+                            @RequestParam("concreteDay") String concreteDay, Model model,
                             RedirectAttributes redirectAttributes) {
         AcademicClassDto classById = academicClassService.getById(agendaDto.getClassId());
+        if (result.hasErrors()) {
+           model.addAttribute("saveAgenda", agendaDto);
+            return "forward:/classes/" + classById.getClassNumber() + "/journal/" + agendaDto.getCourseId();
+        }
+
         if (agendaDto.getClasswork().isBlank() && agendaDto.getTest().isBlank() && agendaDto.getHomework().isBlank()) {
             redirectAttributes.addAttribute("allFieldsBlankMessage", "At least one type has to be chosen");
             redirectAttributes.addAttribute("concreteDay", concreteDay);
-            return "redirect:/classes/" + classById.getClassNumber() + "/journal/" + agendaDto.getCourseId() + "?date=" + agendaDto.getDate();
+            return "redirect:/classes/" + classById.getClassNumber() + "/journal/" + agendaDto.getCourseId();
         }
 
         if (classworkService.getClassWorkOfCourse(LocalDate.parse(agendaDto.getDate()), agendaDto.getClassId(), agendaDto.getCourseId()) != null) {
@@ -65,5 +73,4 @@ public class AgendaOfJournalController {
         }
         return "redirect:/classes/" + classById.getClassNumber() + "/journal/" + agendaDto.getCourseId() + "?date=" + agendaDto.getDate();
     }
-
 }
